b94c142d1fabf39e512715f6b958499320e7ba5e
mench <contact@mench.co>
2015-09-19 23:21:10 -0400

    Add Google login.

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,49 @@
+from google.appengine.api import users
 from django import http
 from django.shortcuts import render_to_response
-from django.template import Context, loader
+from django.http import HttpResponse
+from django.template import Context, loader 
 
 def home(request):
 	return http.HttpResponse('Hello World!')
 
 def beta(request):
-	return render_to_response('beta.html')
+  user = users.get_current_user()
+  if user:
+    context = Context({
+      'user_name': user.nickname(),
+      'login_url': "",
+      'logout_url': users.create_logout_url('beta'),
+      'logged_in': True
+    }, autoescape=False)
+  else:
+    context = Context({
+      'user_name': "",
+      'login_url': users.create_login_url('beta'),
+      'logout_url': "",
+      'logged_in': False
+    }, autoescape=False)
+
+  template = loader.get_template('beta.html')
+  return HttpResponse(template.render(context))  
+
+def test(request):
+  user = users.get_current_user()
+  if user:
+    name = user.nickname()
+    login_url = users.create_logout_url('/')
+    greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+               (user.nickname(), users.create_logout_url('/')))
+  else:
+    name = "friend"
+    login_url = users.create_login_url('/')
+    greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/'))
+
+  template = loader.get_template('test.html')
+  # Context is a normal Python dictionary whose keys can be accessed in the template index.html
+  context = Context({
+    'test': login_url,
+    'name': name
+  })
+  return HttpResponse(template.render(context))
--- myproject/urls.py
+++ myproject/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'beta', 'hello.views.beta'),
+    url(r'test', 'hello.views.test'),    
     url(r'^$', 'hello.views.home'),
 )
